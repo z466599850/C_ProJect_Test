857e49f7ed31d586bbd5c852f3eabfbf0f139ef9
令 <120230459+z466599850@users.noreply.github.com>
2023-02-09 22:43:56 +0800

    Delete text2022-12-29 directory

--- text2022-12-29/text2022-12-29/add.h
+++
@@ -1,9 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-
-#ifndef __ADD_H__ //ifndef 意思没有定义这个文件  如果我定义过这个文件那么条件为假下面的代码全部不执行 如果没有定义过 执行下面的代码
-#define __ADD_H__//define 意思那我定义这个文件吧
-
-//函数声明
-int Add(int x, int y);
-
-#endif

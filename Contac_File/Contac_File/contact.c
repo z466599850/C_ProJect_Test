399d8e4cff077dcd32a31ba03af7f9abf42bcee5
令 <120230459+z466599850@users.noreply.github.com>
2023-02-19 23:30:29 +0800

    Add files via upload

---
+++ Contac_File/Contac_File/contact.c
@@ -0,0 +1,174 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include "contact.h"
+
+void InitContact(struct Contact* ps)
+{
+	ps->data = (struct PerInfo*)malloc(DEFAULT_SZ * sizeof(PeoInfo));
+	if (ps->data == NULL)
+	{
+		return 0;
+	}
+	ps->size = 0;
+	ps->capacity = DEFAULT_SZ;
+}
+
+//检测容量
+void CheckCapacity(struct Contact* ps)
+{
+	if (ps->size == ps->capacity)
+	{
+		//增容
+		struct PerInfo* ptr = (PeoInfo*)realloc(ps->data, (ps->capacity + 2) * sizeof(PeoInfo));
+		if (ptr != NULL)
+		{
+			ps->data = ptr;
+			ps->capacity += 2;
+			printf("增容成功\n");
+		}
+		else
+		{
+			printf("增容失败");
+		}
+	}
+}
+void AddContact(struct Contact* ps)
+{
+	//检测当前通讯录的容量
+	//1.如果满了，就增加空间
+	//2.如果不满，啥事都不干
+	CheckCapacity(ps);
+	//增加数据
+	printf("请输入姓名:>");
+	scanf("%s", ps->data[ps->size].name);
+	printf("请输入年龄:>");
+	scanf("%d", &(ps->data[ps->size].age));
+	printf("请输入性别:>");
+	scanf("%s", ps->data[ps->size].sex);
+	printf("请输入电话:>");
+	scanf("%s", ps->data[ps->size].tele);
+	printf("请输入地址:>");
+	scanf("%s", ps->data[ps->size].addr);
+
+	ps->size++;
+	printf("添加成功\n");
+}
+
+void ShowContact(const struct Contact* ps)
+{
+	if (ps->size == 0)
+	{
+		printf("通讯录为空\n");
+	}
+	else
+	{
+		int i = 0;
+		printf("%-20s\t%-4s\t%-5s\t%-12s\t%-20s\n", "名字", "年龄", "性别", "电话", "地址");//\t 是tab 20s的意思我们要打印20个字符
+		for (i = 0; i < ps->size; i++)
+		{
+			printf("%-20s\t%-4d\t%-5s\t%-12s\t%-30s\n",
+				ps->data[i].name,
+				ps->data[i].age,
+				ps->data[i].sex,
+				ps->data[i].tele,
+				ps->data[i].addr);
+		}
+	}
+}
+
+static int FindByName(const struct Contact* ps, char name[MAX_NAME])//static 加这个是只能在源文件内部看到
+{
+	int i = 0;
+	for (i = 0; i < ps->size; i++)
+	{
+		if (0 == strcmp(ps->data[i].name, name))
+		{
+			return i;
+		}
+	}
+	return -1;//找不到的情况
+}
+
+void DelContact(struct Contact* ps)
+{
+	char name[MAX_NAME];
+	printf("请输入要删除人的名字:>");
+	scanf("%s", name);
+	//1.查找要删除的人在什么位置
+	//找到了返回名字所在元素的下标
+	//找不到返回-1
+	int pos = FindByName(ps, name);//查找名字
+	//2.删除
+	if (pos = -1)
+	{
+		printf("要删除的人不存在\n");
+	}
+	else
+	{
+		int j = 0;
+		for (j = pos; j <ps->size-1 ; j++)
+		{
+			ps->data[j] = ps->data[j + 1];
+		}
+		ps->size--;
+		printf("删除成功\n");
+	}
+}
+
+
+void SearchContact(const struct Contact* ps)
+{
+	char name[MAX_NAME];
+	printf("请输入要查找人的名字:>");
+	scanf("%s", name);
+	int pos = FindByName(ps, name);
+	if (pos == -1)
+	{
+		printf("要查找的人不存在\n");
+	}
+	else
+	{
+		printf("%-20s\t%-4s\t%-5s\t%-12s\t%-20s\n", "名字", "年龄", "性别", "电话", "地址");//\t 是tab 20s的意思我们要打印20个字符
+		printf("%-20s\t%-4d\t%-5s\t%-12s\t%-30s\n",
+			ps->data[pos].name,
+			ps->data[pos].age,
+			ps->data[pos].sex,
+			ps->data[pos].tele,
+			ps->data[pos].addr);
+	}
+}
+
+
+
+void ModfyContact(struct Contact* ps)
+{
+	char name[MAX_NAME];
+	printf("请输入要修改人的名字:>");
+	scanf("%s", name);
+	int pos = FindByName(ps, name);
+	if (pos == -1)
+	{
+		printf("要修改人的信息不存在");
+	}
+	else
+	{
+		printf("请输入姓名:>");
+		scanf("%s", ps->data[pos].name);
+		printf("请输入年龄:>");
+		scanf("%d", &(ps->data[pos].age));
+		printf("请输入性别:>");
+		scanf("%s", ps->data[pos].sex);
+		printf("请输入电话:>");
+		scanf("%s", ps->data[pos].tele);
+		printf("请输入地址:>");
+		scanf("%s", ps->data[pos].addr);
+
+		printf("修改完成\n");
+	}
+}
+
+//释放空间
+void DestrowContact(Contact* ps)
+{
+	free(ps->data);
+	ps->data = NULL;
+}
---
+++ Contac_File/Contac_File/test.c
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include "contact.h"
+void menu()
+{
+	printf("************************************\n");
+	printf("****  1.add           2.del     ****\n");//add 增加 del 删除
+	printf("****  3.search        4.modify  ****\n");//search 查找		modify 修改
+	printf("****  5.show          6.sort    ****\n");//show 显示 sort 排序
+	printf("****  0.exit                    ****\n");//exit 退出
+	printf("************************************\n");
+}
+
+int main()
+{
+	int input = 0;
+	//创建通讯录
+	struct Contact con;//con就是通讯录，里边包含：date指针和size，capecity
+	//初始化通讯录
+	InitContact(&con);//结构体传参最好传地址
+	do
+	{
+		menu();
+		printf("请选择:>");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case ADD:
+			AddContact(&con);
+			break;
+		case DEL:
+			DelContact(&con);
+			break;
+		case SEARCH:
+			SearchContact(&con);
+			break;	
+		case MODIFY:
+			ModfyContact(&con);
+			break;
+		case SHOW:
+			ShowContact(&con);
+			break;
+		case SORT:
+			break;
+		case 0:
+			//销毁通讯录
+			DestrowContact(&con);
+			printf("退出通讯录\n");
+			break;
+		default:
+			printf("选择错误\n");
+			break;
+		}
+	} while (input);
+	return 0;
+}
